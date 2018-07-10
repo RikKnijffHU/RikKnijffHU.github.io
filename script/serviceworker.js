860d279a7a00583f97ac85de9bdc36f141972508
rik.knijff <rik.knijff@student.hu.nl>
2018-07-10 15:02:44 +0200

    11 commit

--- script/serviceworker.js
+++ script/serviceworker.js
@@ -1,7 +1,7 @@
-console.log("versie 6")
+console.log("versie 7")
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      navigator.serviceWorker.register('/script/sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
