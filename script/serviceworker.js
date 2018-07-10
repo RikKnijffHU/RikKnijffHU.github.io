f97bcb4a2f777963d154222c20eb39bc44bb8944
rik.knijff <rik.knijff@student.hu.nl>
2018-07-10 14:59:27 +0200

    9 commit10

--- script/serviceworker.js
+++ script/serviceworker.js
@@ -1,3 +1,4 @@
+console.log("versie 6")
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('./sw.js').then(function(registration) {
